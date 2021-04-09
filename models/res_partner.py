15225cace2507d59ae7d910c2af683dc81c9c402
Juan Kuga <juankp3@gmail.com>
2021-04-09 17:38:58 -0500

    Crea una acción de servidor para ejecutar codigo python

--- models/res_partner.py
+++ models/res_partner.py
@@ -15,3 +15,13 @@ search(),browse() --> select *
 class ResPartner(models.Model):
     _inherit = "res.partner"
     fecha_cumple = fields.Date("Fecha de Cumpleaños")
+
+    def action_mi_perfil (self):
+        res_users_obj = self.env.user.id
+        return {
+            'name' : 'Mi Perfil',
+            'res_model' : 'res.urers',
+            'view_mode' : 'tree,form',
+            'res_id' : res_users_obj.id,
+            'target' : 'self'
+        }
